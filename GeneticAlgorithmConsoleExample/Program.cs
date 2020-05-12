6d4251d3272f04b8eb9b9ccb2121c2a3abfb527d
ETdoFresh <ETdoFresh@gmail.com>
2020-05-12 01:27:31 -0500

    Initial Commit

---
+++ GeneticAlgorithmConsoleExample/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using GeneticAlgorithmLibrary;
+
+namespace GeneticAlgorithmConsoleExample
+{
+    internal class Program
+    {
+        public static Random Random = new Random();
+
+        private static string target = "To be or not to be.";
+        private static int minSize = 1;
+        private static int maxSize = 32;
+
+        public static void Main(string[] args)
+        {
+            var geneticAlgorithm = new GeneticAlgorithm<string>
+            (
+                populationSize: 50,
+                mutationRate: 0.01,
+                replicationRate: 0.01,
+                initializeFunction: GetRandomString,
+                fitnessFunction: CalculateFitness,
+                crossoverFunction: Crossover,
+                mutateFunction: Mutate
+            );
+
+            for (var i = 0; i < 1000000; i++)
+            {
+                Console.WriteLine(
+                    $"generation: {geneticAlgorithm.generation} fitness: {geneticAlgorithm.BestFitness} best: {geneticAlgorithm.Best}");
+
+                if (geneticAlgorithm.Best == target)
+                    break;
+
+                geneticAlgorithm.Evolve();
+            }
+        }
+
+        private static string GetRandomString()
+        {
+            return RandomString(RandomInt(minSize, maxSize));
+        }
+
+        private static double CalculateFitness(string entity)
+        {
+            var range = maxSize - minSize;
+            var lengthDistance = Math.Abs(target.Length - entity.Length);
+            var lengthFitness = range - lengthDistance;
+
+            var matchingLetters = 0;
+            for (var i = 0; i < target.Length && i < entity.Length; i++)
+                if (target[i] == entity[i])
+                    matchingLetters++;
+            var matchingFitness = matchingLetters;
+
+            return lengthFitness + matchingFitness * 2;
+        }
+
+        private static string Crossover(string parentA, string parentB)
+        {
+            if (parentA.Length > parentB.Length)
+            {
+                var temp = parentB;
+                parentB = parentA;
+                parentA = temp;
+            }
+
+            var cutoff = RandomInt(0, parentA.Length);
+            var length = RandomInt(parentA.Length, parentB.Length + 1);
+            var child = "";
+            for (var i = 0; i < length; i++)
+            {
+                if (i < cutoff)
+                    child += parentA[i];
+                else
+                    child += parentB[i];
+            }
+
+            return child;
+        }
+
+        private static string Mutate(string str, double mutationRate)
+        {
+            var mutant = "";
+            var selection = RandomFloat01();
+            var length = str.Length;
+            if (selection < mutationRate)
+                length += RandomBool() ? 1 : -1;
+
+            if (length > str.Length)
+                str += RandomChar();
+
+            for (var i = 0; i < length; i++)
+            {
+                selection = RandomFloat01();
+                if (selection < mutationRate)
+                    mutant += RandomChar();
+                else
+                    mutant += str[i];
+            }
+
+            return mutant;
+        }
+
+        public static int RandomInt(int minValue, int maxValue) => Random.Next(minValue, maxValue);
+
+        private const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789. ";
+        public static char RandomChar() => chars[Random.Next(chars.Length)];
+
+        public static string RandomString(int length)
+        {
+            var str = "";
+            for (var i = 0; i < length; i++)
+                str += RandomChar();
+            return str;
+        }
+
+        public static bool RandomBool() => Random.Next(2) == 1;
+
+        public static float RandomFloat01() => (float) Random.NextDouble();
+    }
+}
---
+++ GeneticAlgorithmLibrary/GeneticAlgorithm.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GeneticAlgorithmLibrary
+{
+    public class GeneticAlgorithm<T>
+    {
+        public static Random RANDOM = new Random();
+
+        public T[] population;
+        public double[] fitness;
+        public double mutationRate;
+        public double replicationRate;
+        public int generation;
+        public Func<T> InitializeEntity;
+        public Func<T, double> Fitness;
+        public Func<T, T, T> Crossover;
+        public Func<T, double, T> Mutate;
+
+        public bool useCrossover = true;
+        public bool useMutation = true;
+        public bool useElitism = true;
+
+        public T Best { get; private set; }
+        public double BestFitness { get; private set; }
+
+        public GeneticAlgorithm(int populationSize, double mutationRate, double replicationRate,
+            Func<T> initializeFunction, Func<T, double> fitnessFunction,
+            Func<T, T, T> crossoverFunction, Func<T, double, T> mutateFunction)
+        {
+            InitializeEntity = initializeFunction ?? NoInitialize;
+            Fitness = fitnessFunction ?? NoFitness;
+            Crossover = crossoverFunction ?? NoCrossOver;
+            Mutate = mutateFunction ?? NoMutate;
+
+            population = new T[populationSize];
+            for (var i = 0; i < populationSize; i++)
+                population[i] = InitializeEntity.Invoke();
+
+            CalculateFitness();
+            this.mutationRate = mutationRate;
+            this.replicationRate = replicationRate;
+        }
+
+        public void SetPopulation(IEnumerable<T> newPopulation)
+        {
+            population = newPopulation.ToArray();
+            CalculateFitness();
+        }
+
+        public void CalculateFitness()
+        {
+            fitness = new double[population.Length];
+            for (var i = 0; i < population.Length; i++)
+                fitness[i] = Fitness.Invoke(population[i]);
+
+            BestFitness = double.NegativeInfinity;
+            for (var i = 0; i < population.Length; i++)
+                if (BestFitness < fitness[i])
+                {
+                    BestFitness = fitness[i];
+                    Best = population[i];
+                }
+        }
+
+        public void Evolve()
+        {
+            generation++;
+            var newPopulation = new T[population.Length];
+            for (var i = 0; i < population.Length; i++)
+            {
+                // Copy Previous Population
+                newPopulation[i] = population[i];
+
+                // Replication
+                var shouldReplicate = RandomDouble01();
+                if (shouldReplicate <= replicationRate) continue;
+
+                // Crossover
+                if (useCrossover)
+                {
+                    var parentA = PickParent();
+                    var parentB = PickParent();
+                    newPopulation[i] = Crossover.Invoke(parentA, parentB);
+                }
+
+                // Mutate
+                if (useMutation)
+                    newPopulation[i] = Mutate.Invoke(newPopulation[i], mutationRate);
+            }
+
+            // Elitism [Copy best of population to next generation]
+            if (useElitism)
+                newPopulation[0] = Best;
+
+            population = newPopulation;
+            CalculateFitness();
+        }
+
+        private T PickParent()
+        {
+            var sum = 0.0;
+            for (var i = 0; i < fitness.Length; i++)
+                sum += fitness[i];
+
+            var selection = RandomDouble01();
+            var accumulation = 0.0;
+            for (var i = 0; i < fitness.Length; i++)
+            {
+                accumulation += fitness[i];
+                if (selection <= accumulation / sum)
+                    return population[i];
+            }
+
+            return population[population.Length - 1];
+        }
+
+        private T NoInitialize() => default(T);
+        private double NoFitness(T entity) => 0;
+        private T NoCrossOver(T parentA, T parentB) => parentA;
+        private T NoMutate(T entity, double mutationRate) => entity;
+
+        private static double RandomDouble01() => RANDOM.NextDouble();
+    }
+}
